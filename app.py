b158541911df1cbb3bbb802b904b215f073ce6b7
tynannosaurus <147985659+tynannosaurus@users.noreply.github.com>
2024-07-05 10:47:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import pandas as pd
-import numpy as np
-from sklearn.utils import resample
-from sklearn.ensemble import GradientBoostingRegressor
 
 app = Flask(__name__)
 
@@ -20,11 +17,6 @@ def predict():
     df = pd.DataFrame([data])
     prediction = model.predict(df)
     return jsonify({'prediction': prediction.tolist()})
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-    })
 
 if __name__ == '__main__':
     app.run(debug=True)
